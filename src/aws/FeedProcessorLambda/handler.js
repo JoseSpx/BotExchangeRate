1426ef4d01fcbe97bce2d929ee4a92567ee110ba
josespx <jose95sp@outlook.com>
2022-02-04 09:19:28 -0500

    added sns config sns publisher

--- src/aws/FeedProcessorLambda/handler.js
+++ src/aws/FeedProcessorLambda/handler.js
@@ -1,7 +1,31 @@
 'use strict';
+const AWS = require('aws-sdk');
 const logger = require('pino')();
+const SNS = new AWS.SNS();
+
+const { topicArn } = process.env;
 
 module.exports.processorData = async (event) => {
-  logger.info(`Data Event : ${JSON.stringify(event)}`);
-  logger.info(event);  
+  logger.info('Event:');
+  logger.info(event);
+  
+  const records = event.Records;
+  records.forEach(item => {
+    if (item.eventName !== 'INSERT') return;
+    const data = item.dynamodb.NewImage;
+    logger.info('Exchange Data');
+    logger.info(data);
+
+    SNS.publish({
+      Message: JSON.stringify(data),
+      TopicArn: topicArn
+    }, (err, data) => {
+      if (err) {
+        logger.error(err.stack);
+        return;
+      }
+      logger.info('push sent');
+      logger.info(data);
+    })
+  })
 };
